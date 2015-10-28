ac05a2143d667a9a33a0d03b4800a9fd555024d7
Mariama <mcso@cin.ufpe.br>
2015-10-28 12:51:42 -0300

    oversample done

--- oversample.m
+++ oversample.m
@@ -1,1 +1,92 @@
-function [training, validation, test] = oversample(data)
+function [training,trainingClass, validation, validationClass, test, testClass] = oversample(data, class)
+
+%Divide the dataset into two classes
+a=class==0;
+b=class==1;
+
+data0= data(a, :);
+data1= data(b, :);
+
+%Divide the class0 in training 50%, validation 25% and test 25%
+
+[numberRows0,~]=size(data0);
+
+nRowsTrn0= fix(0.5*numberRows0);
+nRowsVdt0= fix(0.25*numberRows0);
+
+startVld= nRowsTrn0+1;
+startTest= nRowsTrn0+nRowsVdt0+2;
+
+training0= data0(1:nRowsTrn0,:);
+validation0= data0((startVld):(startTest-1),:);
+test0= data0(startTest:end,:);
+
+%Divide the class1 in training 50%, validation 25% and test 25%
+%Remember class 1 is smaller than 0 so we have to balance it!
+
+[numberRows1,~]=size(data1);
+
+nRowsTrn1= fix(0.5*numberRows1);
+nRowsVdt1= fix(0.25*numberRows1);
+
+startVld= nRowsTrn1+1;
+startTest= nRowsTrn1+nRowsVdt1+2;
+
+training1= data1(1:nRowsTrn1,:);
+validation1= data1((startVld):(startTest-1),:);
+test1= data1(startTest:end,:);
+
+
+
+%Balancing class1 in order to be the same size as class0
+%Training
+[sizeTraining0,~]=size(training0);
+[sizeTraining1,~]=size(training1);
+nReplications= fix((sizeTraining0-sizeTraining1)/sizeTraining1);
+training1=repmat(training1, nReplications,1);
+[auxSize,~]=size(training1);
+incr=sizeTraining0-auxSize;
+training1= [training1; training1((1:incr),:)];
+
+%Validation
+[sizeValidation0,~]=size(validation0);
+[sizeValidation1,~]=size(validation1);
+nReplications= fix((sizeValidation0-sizeValidation1)/sizeValidation1);
+validation1=repmat(validation1, nReplications,1);
+[auxSize,~]=size(validation1);
+incr=sizeValidation0-auxSize;
+validation1= [validation1; validation1((1:incr),:)];
+
+%Merging training, validation and test (class0+class1)
+%Training
+[nRows0, ~]=size (training0);
+[nRows1, ~]=size (training1);
+
+trainingClass=[zeros(nRows0,1);ones(nRows1,1)];
+training=[training0;training1];
+%Shuffling training set
+random=randperm(size(training,1));
+training= training(random,:);
+trainingClass=trainingClass(random,:);
+
+%Validation
+[nRows0, ~]=size (validation0);
+[nRows1, ~]=size (validation1);
+
+validationClass=[zeros(nRows0,1);ones(nRows1,1)];
+validation=[validation0;validation1];
+%Shuffling validation set
+random=randperm(size(validation,1));
+validation= validation(random,:);
+validationClass= validationClass(random,:);
+
+%Test
+[nRows0, ~]=size (test0);
+[nRows1, ~]=size (test1);
+testClass=[zeros(nRows0,1);ones(nRows1,1)];
+test=[test0; test1];
+end
+
+
+
+
--- redesNeuraisMain.m
+++ redesNeuraisMain.m
@@ -1,6 +1,14 @@
 %Read file: Mamography 
-originalData= csvread(mammography-consolidated.csv);
+originalData= csvread('mammography-consolidated.csv');
 
+%Put the class atribute into another matrix
+class = originalData(:, end);
+data= originalData(:, 1:(end-1));
 
 %Divide the dataset in training 50%, validation 25%, test 25%
-[training, validation, test]=oversample(data);
+[training,trainingClass, validation, validationClass, test, testClass]=oversample(data, class);
+
+%Write balanced data into a csv file
+csvwrite('mammography-consolidated-training-oversample.csv',[training, trainingClass])
+csvwrite('mammography-consolidated-validation-oversample.csv',[validation, validationClass])
+csvwrite('mammography-consolidated-test-oversample.csv',[test, testClass])
